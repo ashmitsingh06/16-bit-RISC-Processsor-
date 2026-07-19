c13ad711cbe4288c34145b07abc717c74145a194
ashmitsingh06 <ashmitsinghwork06@gmail.com>
2026-07-19 10:03:12 +0530

    Add files via upload

---
+++ simulation/main_test.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10.12.2024 22:12:58
+// Design Name: 
+// Module Name: main_test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module main_test(
+
+    );
+endmodule
